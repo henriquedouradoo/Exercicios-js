3f4364b80b3f49f1d3f082ddcc0ad708fe79608b
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-08-04 10:15:53 -0300

    Update Principios.js

--- Principios.js
+++ Principios.js
@@ -2,3 +2,4 @@ console.log('Meu nome é Henrique');
 console.log(typeof 5);
 console.log(12 + 9);
 console.log(`A multiplicação de 6 por 6 é ${6*6}`);
+console.log('Meu ' + 'nome ' + 'é ' + 'Henrique');
