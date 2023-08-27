cac51c802f130b5ee547e0dee013946a6d6cfeeb
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-08-26 23:29:24 -0300

    Delete Exercícios/04 directory

--- Exercícios/04/index.js
+++
@@ -1,9 +0,0 @@
-const listItems = document.querySelectorAll('li');
-
-const handleHover = (event) => {
-    return event.target.innerText = 'ON';
-};
-
-if(listItems.length > 1) {
-    listItems.forEach(item => item.addEventListener('mouseover', handleHover));
-};
