f0b318fef27ad462219b1e52fd4589ec04c5f89a
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-08-26 23:25:01 -0300

    Delete Exercícios/03 directory

--- Exercícios/03/index.js
+++
@@ -1,9 +0,0 @@
-
-
-const buttonElem = document.querySelector("#wrapper button");
-const inputElem = document.querySelector("#wrapper input");
-
-buttonElem.addEventListener('click'), () => {
-    const oldText = inputElem.value;
-    return inputElem.value = oldText === "ON" ? "OFF" : "ON";
-};
